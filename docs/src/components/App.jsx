69a398c09e7ac4989f65be32d8804b2b6ef03fd9
arnavgupta1209 <arnavgupta1209@gmail.com>
2024-02-01 02:09:17 +0530

    redid a lot

--- docs/src/components/App.jsx
+++ docs/src/components/App.jsx
@@ -10,15 +10,16 @@ export default function App() {
   for (let i=0; i<9; i++){
     pokeclicked[i] = false;
   }
-  
+
   let [state, setState] = useState({pokemon_list, score, highscore, clickedpokemon, pokeclicked});
 
 
   return (
     <>
       <div className="header">
-            <h1>Memory Game</h1>
-            <h2>Score: {state.score} Highscore: {state.highscore}</h2>
+            <h2>Memory Game</h2>
+            <h4>Score: {state.score} Highscore: {state.highscore}</h4>
+            <p>Keep clicking on pokemon but dont click on the same one twice!</p>
         </div>
       <Gamegrid pokemon_list = {state.pokemon_list} setState={setState} state={state}/>
     </>
