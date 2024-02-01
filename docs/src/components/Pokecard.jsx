ea56fb7d3ac01c008ff49762d0aefa11687df529
arnavgupta1209 <arnavgupta1209@gmail.com>
2024-02-01 13:56:01 +0530

    fixed condition

--- docs/src/components/Pokecard.jsx
+++ docs/src/components/Pokecard.jsx
@@ -27,7 +27,14 @@ export default function Pokecard({pokemon, setState, state}) {
                 console.log("here");
                 newstate.pokeclicked[id] = true;
                 if (newstate.score+1 > newstate.highscore){newstate.highscore = newstate.score+1}
-                if (newstate.score+1 == 9){newstate.highscore = 0; newstate.score = 0; alert("Max Score Reached!")}
+                if (newstate.score+1 == 9){
+                    newstate.highscore = 0; 
+                    newstate.score = 0; 
+                    alert("Max Score Reached!");
+                    for (let i=0; i<9; i++){
+                        newstate.pokeclicked[i] = false;
+                    }
+                }
                 newstate.score += 1;
                 newstate.clickedpokemon = id;
                 newstate.pokemon_list = shuffle(state.pokemon_list);
